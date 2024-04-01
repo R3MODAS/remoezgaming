8726cc21d31c345253c1c8c5a2514ef11b018a66
Sharadindu Das <remodas7774@gmail.com>
2024-04-01 12:02:08 +0530

    updated my website's file

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,13 +1,17 @@
+import { useParams } from "react-router-dom"
 import ConfigInfo from "../utils/info.json"
 
 const Config = () => {
+  const { id } = useParams()
+  const ConfigContent = ConfigInfo.filter((config) => config.id === parseInt(id))
+
   return (
     <div className='mt-28 container mx-auto'>
       {
-        ConfigInfo?.map((config) => (
+        ConfigContent?.map((config) => (
           <div key={config?.id} className="text-center flex flex-col justify-center items-center gap-2 sm:px-0 w-full">
             <video src={config?.video} loop controls muted className="w-[1000px]" />
-            <h2 className="sm:text-2xl text-xl font-PoppinsSemi mt-2 sm:mb-1 mb-2">{config?.name}</h2>
+            <h2 className="sm:text-2xl text-xl font-PoppinsSemi mt-2 sm:mb-1 mb-2 px-10">{config?.name}</h2>
             <a href={config?.config_link} target="_blank" className="bg-red-500 text-white sm:px-5 px-4 h-9 sm:h-11 flex items-center justify-center font-PoppinsMed rounded-lg sm:text-base text-sm">Config Link</a>
           </div>
         ))
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import Config from "../utils/config.json"
 
 const Home = () => {
   return (
     <div className='mt-28 container mx-auto px-4'>
-      <h1 className='font-PoppinsBold text-3xl'>Config Files</h1>
-      <div className='config-cards mt-8 flex items-center'>
+      <h1 className='font-PoppinsBold text-3xl sm:text-start text-center'>Config Files</h1>
+      <div className='config-cards mt-8 flex items-center gap-8 flex-wrap sm:justify-start justify-center'>
         {
           Config.map((item) => (
-            <Link key={item?.id} to={`/config/${item?.id}`} className="config-card hover:scale-95 transition-all w-80 cursor-pointer">
-              <img className='w-full h-full rounded-lg' src={item?.thumbnail} alt="evil-within" />
+            <Link key={item?.id} to={`/config/${item?.id}`} className="config-card hover:scale-95 transition-all cursor-pointer">
+              <div className='sm:w-96 w-80 sm:h-56 rounded-lg'>
+                <img loading='lazy' className='w-full h-full rounded-lg object-cover' src={item?.thumbnail} alt="evil-within" />
+              </div>
               <h2 className='font-PoppinsSemi text-base mt-2'>{item?.name}</h2>
             </Link>
           ))
