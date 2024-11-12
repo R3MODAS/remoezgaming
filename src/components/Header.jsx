e86a99c1ded52c52ecc87112cca2cc119c674b94
Sharadindu Das <remodas7774@gmail.com>
2024-11-12 12:36:57 +0530

    feat: added the config files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="shadow-md bg-[#217dc9] fixed left-0 right-0 top-0 w-full z-10 h-20 px-4">
-        <div className="container mx-auto flex justify-between items-center h-full">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <Link to="/">
           <div className="flex items-center gap-4">
-          <Link to="/">
-            <img src="/assets/logo.png" alt="logo" className="rounded-full w-16" />
-          </Link>
-            <span className="font-GreatVibesReg text-3xl font-medium">Remo Ez Gaming</span>
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              className="rounded-full w-16"
+            />
+            <span className="font-GreatVibesReg text-3xl font-medium">
+              Remo Ez Gaming
+            </span>
           </div>
+        </Link>
 
-            <ul className="flex items-center gap-5 font-PoppinsMed text-base">
-                <li>
-                 <Link to="/">Home</Link> 
-                </li>
-                <li>
-                 <Link to="/about">About</Link> 
-                </li>
-            </ul>
-        </div>
+        <ul className="flex items-center gap-5 font-PoppinsMed text-base">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,31 @@
-import { Link } from 'react-router-dom'
-import Config from "../utils/config.json"
+import { Link } from "react-router-dom";
+import Config from "../utils/config.json";
 
 const Home = () => {
   return (
-    <div className='mt-28 container mx-auto px-4 mb-10'>
-      <h1 className='font-GreatVibesReg text-6xl text-center'>Config files</h1>
-      <div className='config-cards mt-12 flex items-center gap-12 flex-wrap justify-center'>
-        {
-          Config.map((item) => (
-            <Link key={item?.id} to={`/config/${item?.id}`} className="config-card hover:scale-95 transition-all cursor-pointer">
-              <div className='sm:w-[400px] w-80 sm:h-56 rounded-lg'>
-                <img loading='lazy' className='w-full h-full rounded-lg object-cover' src={item?.thumbnail} alt="evil-within" />
-              </div>
-              <h2 className='font-PoppinsSemi text-base mt-2'>{item?.name}</h2>
-            </Link>
-          ))
-        }
+    <div className="mt-28 container mx-auto px-4 mb-10">
+      <h1 className="font-GreatVibesReg text-6xl text-center">Config files</h1>
+      <div className="config-cards mt-12 flex items-center gap-12 flex-wrap justify-center">
+        {Config.map((item) => (
+          <Link
+            key={item.id}
+            to={`/config/${item.id}`}
+            className="config-card hover:scale-95 transition-all cursor-pointer"
+          >
+            <div className="sm:w-[400px] w-80 sm:h-56 rounded-lg">
+              <img
+                loading="lazy"
+                className="w-full h-full rounded-lg object-cover"
+                src={item.thumbnail}
+                alt={item.name}
+              />
+            </div>
+            <h2 className="font-PoppinsSemi text-base mt-2">{item?.name}</h2>
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
